ecaf71bc6ca45de00880ee7255a6e55db0460786
Daniel Kaczorek <danielkaczorek@outlook.com>
2017-06-07 19:40:25 +0100

    App

---
+++ MVC-Mayborn/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public void AjaxMethod(string FirstName, string LastName)
+        {
+
+            var userData = "Firstname: " + FirstName + "," + "Lastname: " + LastName + Environment.NewLine;
+
+            var dataFile = Server.MapPath("~/App_Data/data.txt");
+
+            System.IO.File.AppendAllText(dataFile, userData);
+
+        }
+    }
+}
---
+++ MVC-Mayborn/scripts/Index.js
@@ -0,0 +1,32 @@
+﻿$(function () {
+
+    $("#button").click(function () {
+
+        var firstName = $("#firstName").val();
+        var lastName = $("#lastName").val();
+
+        $("#someTextArea").append("First Name: " + firstName + "\n Lastname: " + lastName + " \n ");
+
+        SaveToFile(firstName, lastName);
+    });
+
+
+    function SaveToFile(firstname, lastname) {
+        $.ajax({
+            type: "POST",
+            url: "/Home/AjaxMethod",
+            data: "{'FirstName':'" + firstname + "', 'LastName':'" + lastname + "'}",
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            success: function (response) {
+                alert("Success");
+            },
+            failure: function (response) {
+                alert(response.responseText);
+            },
+            error: function (response) {
+                alert(response.responseText);
+            }
+        });
+    }
+});
